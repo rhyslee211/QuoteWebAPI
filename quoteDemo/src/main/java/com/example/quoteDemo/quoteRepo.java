ed8cb42b44bf2fd6827bbd7af89866e0b57b1811
rhyslee211 <rhyslee211@gmail.com>
2024-02-12 08:42:49 -0500

    changed repository structure

--- quoteDemo/src/main/java/com/example/quoteDemo/quoteRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.restservice;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface quoteRepo extends CrudRepository<quote, Long> {
-    
-}
